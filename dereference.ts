53e8a58878494dc5a383b95b0ee2b3bf49adbd89
jatin <sandilya.jatin@gmail.com>
2024-07-16 17:40:57 +0530

    feat: add script to dereference oas spec

---
+++ dereference.ts
@@ -0,0 +1,40 @@
+import SwaggerParser from "@apidevtools/swagger-parser";
+import fs from "fs";
+import path from "path";
+
+const specs = [
+  {
+    inputFile: path.join(__dirname, "api/api.yaml"),
+    outputFile: path.join(__dirname, "api/generated/api.json"),
+  },
+  {
+    inputFile: path.join(__dirname, "api/read.yaml"),
+    outputFile: path.join(__dirname, "api/generated/read.json"),
+  },
+  {
+    inputFile: path.join(__dirname, "api/write.yaml"),
+    outputFile: path.join(__dirname, "api/generated/write.json"),
+  },
+];
+
+async function deRefOpenApiSpecs() {
+  try {
+    specs.forEach(async (spec) => {
+      const api = await SwaggerParser.dereference(spec.inputFile, {
+        resolve: {
+          external: true,
+        },
+        dereference: {
+          circular: false,
+        },
+      });
+      const deRefSpec = JSON.stringify(api, null, 2);
+      fs.writeFileSync(spec.outputFile, deRefSpec);
+      console.log(`Deref'd OpenAPI spec saved to ${spec.outputFile}`);
+    });
+  } catch (error) {
+    console.error(`Error dereferencing OpenAPI spec: ${error.message}`);
+  }
+}
+
+deRefOpenApiSpecs();
