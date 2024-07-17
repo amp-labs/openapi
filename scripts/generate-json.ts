5915bea92d0c9d8c53015b4bbc14ba5d0b81eb18
jatin <sandilya.jatin@gmail.com>
2024-07-17 13:03:14 +0530

    chore: cleanup w/ better naming

--- scripts/generate-json.ts
+++ scripts/generate-json.ts
@@ -1,19 +1,26 @@
+/**
+ * Note: the openapi specs in api/generated are used for auto-generation of documentation
+ * and do not contain $ref attributes as the schema references are resolved at conversion
+ *
+ * Ref: https://apitools.dev/swagger-parser/docs/swagger-parser.html#dereferenceapi-options-callback
+ */
+
 import SwaggerParser from "@apidevtools/swagger-parser";
 import fs from "fs";
 import path from "path";
 
 const specs = [
   {
-    inputFile: path.join(__dirname, "api/api.yaml"),
-    outputFile: path.join(__dirname, "api/generated/api.json"),
+    inputFile: path.join(__dirname, "../api/api.yaml"),
+    outputFile: path.join(__dirname, "../api/generated/api.json"),
   },
   {
-    inputFile: path.join(__dirname, "api/read.yaml"),
-    outputFile: path.join(__dirname, "api/generated/read.json"),
+    inputFile: path.join(__dirname, "../api/read.yaml"),
+    outputFile: path.join(__dirname, "../api/generated/read.json"),
   },
   {
-    inputFile: path.join(__dirname, "api/write.yaml"),
-    outputFile: path.join(__dirname, "api/generated/write.json"),
+    inputFile: path.join(__dirname, "../api/write.yaml"),
+    outputFile: path.join(__dirname, "../api/generated/write.json"),
   },
 ];
 
